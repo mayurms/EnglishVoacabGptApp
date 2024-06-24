3108ba2f65e8ff66ca3c0f8c9f5c4cfab1ab2591
mayur <mayurms5060@gmail.com>
2024-06-24 13:44:56 +0530

    we create only fragment

---
+++ app/src/main/java/com/example/englishvocabgpt/AboutusFragment.kt
@@ -0,0 +1,19 @@
+package com.example.englishvocabgpt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AboutusFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_aboutus, container, false)
+    }
+
+}
